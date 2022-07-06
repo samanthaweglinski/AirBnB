d2ed295454118f30111f7efec4e5ebb96c32970e
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-06 15:43:39 -0700

    Finished log in

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -35,20 +35,35 @@ router.post("/", validateLogin, async (req, res, next) => {
   const user = await User.login({ credential, password });
 
   if (!user) {
-    const err = new Error("Login failed");
-    err.status = 401;
-    err.title = "Login failed";
-    err.errors = ["The provided credentials were invalid."];
-    return next(err);
+    res.status(401);
+    return res.json({
+      message: 'Invalid credentials',
+      statusCode: 401
+    });
   }
 
-  const token = await setTokenCookie(res, user);
-  console.log(user)
+  if (!req.body) {
+    res.json( {
+      message: "Validation error",
+      statusCode: 400,
+      errors: {
+        email: "Email is Required",
+        password: "Password is Required"
+      }
+    })
+  };
 
-  return res.json({
-    user,
-    token
-  });
+  const token = await setTokenCookie(res, user);
+
+  const userRes = {
+    id: req.user.id,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    email: req.user.email,
+    token: token,
+  };
+
+  return res.json(userRes);
 });
 
 // Log out
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,7 +1,7 @@
 // backend/utils/auth.js
-const jwt = require('jsonwebtoken');
-const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const jwt = require("jsonwebtoken");
+const { jwtConfig } = require("../config");
+const { User } = require("../db/models");
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -17,11 +17,11 @@ const setTokenCookie = (res, user) => {
   const isProduction = process.env.NODE_ENV === "production";
 
   // Set the token cookie
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     maxAge: expiresIn * 1000, // maxAge in milliseconds
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction && "Lax"
+    sameSite: isProduction && "Lax",
   });
 
   return token;
@@ -39,13 +39,13 @@ const restoreUser = (req, res, next) => {
 
     try {
       const { id } = jwtPayload.data;
-      req.user = await User.scope('currentUser').findByPk(id);
+      req.user = await User.scope("currentUser").findByPk(id);
     } catch (e) {
-      res.clearCookie('token');
+      res.clearCookie("token");
       return next();
     }
 
-    if (!req.user) res.clearCookie('token');
+    if (!req.user) res.clearCookie("token");
 
     return next();
   });
@@ -55,11 +55,11 @@ const restoreUser = (req, res, next) => {
 const requireAuth = function (req, _res, next) {
   if (req.user) return next();
 
-  const err = new Error('Unauthorized');
-  err.title = 'Unauthorized';
-  err.errors = ['Unauthorized'];
+  const err = new Error("Unauthorized");
+  err.title = "Unauthorized";
+  err.errors = ["Unauthorized"];
   err.status = 401;
   return next(err);
-}
+};
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
