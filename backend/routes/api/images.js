f5ab5041e3b7545dd3e52523f0e3acbf4c0473f6
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-08 15:06:34 -0700

    Finished delete an image

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -5,7 +5,31 @@ const router = express.Router();
 
 // Delete an Image
 router.delete('/:imageId', requireAuth, async (req, res) => {
-  
+  const image = await Image.findByPk(req.params.imageId);
+
+  if (!image) {
+    res.status(404);
+    res.json({
+      message: "Image couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  if (image.imageableId !== req.user.id) {
+    res.status(403);
+    res.json({
+      message: "Image must belong to current user in order to delete",
+      statusCode: 403,
+    });
+  }
+
+  image.destroy();
+  image.save();
+
+  res.json({
+    message: "Successfully deleted",
+    statusCode: 200,
+  });
 })
 
 
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,44 +1,50 @@
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const propertiesRouter = require('./properties.js');
-const reviewsRouter = require('./reviews.js');
-const bookingsRouter = require('./bookings.js');
-const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const propertiesRouter = require("./properties.js");
+const reviewsRouter = require("./reviews.js");
+const bookingsRouter = require("./bookings.js");
+const imagesRouter = require("./images");
+const {
+  restoreUser,
+  requireAuth,
+  setTokenCookie,
+} = require("../../utils/auth.js");
 const { Property, Review, Image, User, sequelize } = require("../../db/models");
 
 // GET /api/restore-user (must be connected before any other middleware or route handlers)
-router.get('/restore-user', (req, res) => {
-    return res.json(req.user);
-  });
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
 
-router.get('/set-token-cookie', async (_req, res) => {
+router.get("/set-token-cookie", async (_req, res) => {
   const user = await User.findOne({
     where: {
-      username: 'Demo-lition'
-    }
+      username: "Demo-lition",
+    },
   });
   setTokenCookie(res, user);
   return res.json({ user });
 });
 
-router.post('/test', function(req, res) {
+router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
 
-router.get('/require-auth', requireAuth, (req, res) => {
+router.get("/require-auth", requireAuth, (req, res) => {
   return res.json(req.user);
 });
 
 router.use(restoreUser);
 
-router.use('/session', sessionRouter);
-router.use('/users', usersRouter);
-router.use('/properties', propertiesRouter);
-router.use('/reviews', reviewsRouter);
-router.use('/bookings', bookingsRouter)
+router.use("/session", sessionRouter);
+router.use("/users", usersRouter);
+router.use("/properties", propertiesRouter);
+router.use("/reviews", reviewsRouter);
+router.use("/bookings", bookingsRouter);
+router.use("/images", imagesRouter);
 
-router.post('/test', (req, res) => {
+router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
 
