ed38f5401081f6e4305e36ad5cc1f98ce6bb1dd7
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-24 16:01:04 -0700

    Finished creating signup thunk, still need to work on nested user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,8 +32,27 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+// restoreUser thunk
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
+
+// signup thunk
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    }),
+  });
   const data = await response.json();
   dispatch(setUser(data));
   return response;
@@ -58,3 +77,5 @@ const sessionReducer = (state = initialState, action) => {
 };
 
 export default sessionReducer;
+
+
