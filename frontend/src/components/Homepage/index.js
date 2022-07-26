bedd63ebf6afaef03148883c5a990aed2e982ce2
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-26 12:49:23 -0700

    Added reviews to homepage

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -22,18 +22,21 @@ const Homepage = () => {
       </div>
       <div>
         {allProps.allProperties.map((ele) => (
-          <div key={ele.id}>
-            <h3>{ele.name}</h3>
-            <h4>
-              {ele.city}, {ele.state}
-            </h4>
-            <img
-              src={ele.previewImage}
-              alt={ele.name}
-            ></img>
-            <p>{ele.description}</p>
-            <p> Price: ${ele.price}</p>
-          </div>
+          <Link to={`/properties/${ele.id}`}>
+            <div key={ele.id}>
+              <h3>{ele.name}</h3>
+              <h4>
+                {ele.city}, {ele.state}
+              </h4>
+              <img src={ele.previewImage} alt={ele.name}></img>
+              <p>{ele.description}</p>
+              <p> Price: ${ele.price}/night</p>
+              <div className="property-rating">
+                <i className="fa-solid fa-star"></i>
+                <div className="rating-score">New, if no reviews</div>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </>
