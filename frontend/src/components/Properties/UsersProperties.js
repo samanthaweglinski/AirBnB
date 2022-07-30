b1d63a7fc28a021e86ac48a533a9cae5e5a204fa
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-29 22:07:50 -0700

    Everything working, deleted extra code from UsersProperties.js. Starting get currentusers reviews

--- frontend/src/components/Properties/UsersProperties.js
+++ frontend/src/components/Properties/UsersProperties.js
@@ -1,30 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getCurrentUserProperties } from "../../store/property";
-import PropertyDetails from "./PropertyDetails";
 import "./Properties.css";
 
 const UsersProperties = () => {
   const dispatch = useDispatch();
   const userPropertiesObj = useSelector((state) => state.properties);
   const userProperties = Object.values(userPropertiesObj); //changing to array to .map
-  const [loaded, setLoaded] = useState(false);
   // console.log("prop:", userProperties);
 
   useEffect(() => {
-    dispatch(getCurrentUserProperties()).then(() => setLoaded(true));
+    dispatch(getCurrentUserProperties());
   }, [dispatch]);
 
   return (
     <div>
       <h2>My Properties</h2>
-      {/* {loaded &&
-        userProperties.map((ele) => (
-          <Link to={`/properties/${ele.id}`} key={ele.id}>
-            <PropertyDetails hideButtons key={ele.id} passedPropId={ele.id} />
-          </Link>
-        ))} */}
+
       {userProperties.map((ele) => (
           <Link to={`/properties/${ele.id}`} key={ele.id}>
             <div key={ele.id}>
