b66dea6865d128d03855ac98ba278818e3aefbad
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-07-31 23:32:58 -0700

    Added better buttons to navbar

--- frontend/src/components/DemoUser/index.js
+++ frontend/src/components/DemoUser/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import './DemoUser.css'
 
 export default function DemoUser() {
   const dispatch = useDispatch();
@@ -14,7 +15,8 @@ export default function DemoUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <button type="submit">Demo</button>
+      {/* <button type="submit">Demo</button> */}
+      <button class="button-23" type="submit">Demo</button>
     </form>
   );
 }
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import SignupForm from "./SignupForm";
+import "./SignupForm.css"
 
 function SignupFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Sign Up</button>
+      <button className="button-23" onClick={() => setShowModal(true)}>Sign Up</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <SignupForm />
