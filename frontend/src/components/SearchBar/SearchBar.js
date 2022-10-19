d6a4549d04998dc59d4a619b9fbe1a36e5ed2d72
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-10-19 14:23:54 -0700

    Search filter working

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css"
 
-function SearchBar({placeholder, data}) {
+function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([])
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value
+    const newFilter = data.filter((value) => {
+      return value.name.includes(searchWord)
+    })
+    setFilteredData(newFilter)
+  }
+
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} />
+        <input type="text" placeholder={placeholder} onChange={handleFilter}/>
         <div className="searchIcon">
           <i class="fa-solid fa-magnifying-glass"></i>
         </div>
       </div>
+      { (filteredData.length != 0) && (
       <div className="dataResult">
-        {data.map((value, key) => {
+        {filteredData.map((value, key) => {
           return <a className="dataItem" href={`/properties/${value.id}`}>
             <p>{value.name}</p>
           </a>
         })}
       </div>
+      )}
     </div>
   )
 }
