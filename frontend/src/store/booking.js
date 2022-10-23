5837a8f70e6415d5aca3cd3fef3e5695d3f41e73
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-10-23 13:12:37 -0700

    Creating bookings store

---
+++ frontend/src/store/booking.js
@@ -0,0 +1,7 @@
+import { csrfFetch } from "./csrf";
+
+const GET_ALL_BOOKINGS = "bookings/GET_ALL_BOOKINGS";
+const FIND_BOOKING = "bookings/GET_PROPERTY_BOOKINGS";
+const CREATE_BOOKING = "bookings/CREATE_BOOKING";
+const UPDATE_BOOKING = "bookings/UPDATE_BOOKING";
+const DELETE_BOOKING = "bookings/DELETE_BOOKING";
