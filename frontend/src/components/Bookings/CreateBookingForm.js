d74432d67e8fda7fde7c64ddfbcbfefc601eaee8
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-10-23 19:33:11 -0700

    Working on create booking form

---
+++ frontend/src/components/Bookings/CreateBookingForm.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { createBooking, getAllBookings } from "../../store/booking";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import LoginFormModal from "../LoginFormModal";
+
+const BookingForm = ({ property, star, review, booking }) => {
+
+}
+
+export default BookingForm
