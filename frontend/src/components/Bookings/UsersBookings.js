3d872ffd6a2229ae95961e1fadb525ae9c9fe1e5
Samantha Weglinski <samantha.weglinski@gmail.com>
2022-10-24 00:12:38 -0700

    Fixed issue with previewImage loading for myBookings page

--- frontend/src/components/Bookings/UsersBookings.js
+++ frontend/src/components/Bookings/UsersBookings.js
@@ -2,19 +2,24 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getUserBookings, deleteBooking } from "../../store/booking";
+import { listAllProperties } from "../../store/property";
 import "./UsersBookings.css";
 
 function UsersBookings() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const userBookingsObj = useSelector((state) => state.bookings);
+  const userBookingsObj = useSelector((state) => state?.bookings);
   const userBookings = Object.values(userBookingsObj);
   const [isLoaded, setIsloaded] = useState(false);
-  const properties = useSelector((state) => state.properties);
+  const properties = useSelector((state) => state?.properties);
 
   useEffect(() => {
     dispatch(getUserBookings()).then(() => setIsloaded(true));
   }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(listAllProperties())
+  }, [dispatch])
 
   const removeBooking = (bookingId) => async (e) => {
     e.preventDefault();
@@ -33,10 +38,10 @@ function UsersBookings() {
         <h2 className="my_bookings_title">My Bookings</h2>
         <div className="users_bookings_container">
           {userBookings.map((booking) => (
-            <div key={booking.id} className="individual_booking">
+            <div key={booking?.id} className="individual_booking">
               <div className="booking_property_picture">
                 <img
-                  src={properties[booking.propertyId].previewImage}
+                  src={properties[booking?.propertyId]?.previewImage}
                   alt="previewimage"
                 />
               </div>
@@ -44,20 +49,20 @@ function UsersBookings() {
                 <div className="booking_information">
                   <div className="booking_prop_info">
                     <div className="booking_property_name">
-                      {properties[booking.propertyId].name}
+                      {properties[booking?.propertyId]?.name}
                     </div>
                     <div className="booking_property_address">
-                      {properties[booking.propertyId].address}
+                      {properties[booking?.propertyId]?.address}
                     </div>
                   </div>
                   <div>
-                    <div>Start Date: {booking.startDate}</div>
-                    <div>End Date: {booking.endDate}</div>
+                    <div>Start Date: {booking?.startDate}</div>
+                    <div>End Date: {booking?.endDate}</div>
                   </div>
                 </div>
                 <div>
                   <button
-                    onClick={removeBooking(booking.id)}
+                    onClick={removeBooking(booking?.id)}
                     className="delete-booking-button"
                   >
                     Delete Booking
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink, useHistory, Link } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 
 function ProfileButton({ user }) {
@@ -33,28 +33,56 @@ function ProfileButton({ user }) {
   const showUsersBookings = (e) => {};
 
   return (
+    // <>
+    //   <div className="actions_button">
+    //     <button className="actions_menu" onClick={openMenu}>
+    //       <i className="fas fa-bars nav_bars_icon"></i>
+    //       <i className="fas fa-user-circle user_icon"></i>
+    //     </button>
+    //     {showMenu && (
+    //       <div id="menu">
+    //         <div className="profile_options">
+    //           <NavLink to="/currentUser/properties">
+    //             <button onClick={showUsersProperties}>My Properties</button>
+    //           </NavLink>
+    //           <NavLink to="/currentUser/reviews">
+    //             <button onClick={showUsersReviews}>My Reviews</button>
+    //           </NavLink>
+    //           <NavLink to="/currentUser/bookings">
+    //             <button onClick={showUsersBookings}>My Bookings</button>
+    //           </NavLink>
+    //           <button onClick={logout}>Log Out</button>
+    //         </div>
+    //         <p>Hello {user.username}!</p>
+    //         <p>Email: {user.email}</p>
+    //       </div>
+    //     )}
+    //   </div>
+    // </>
     <>
       <div className="actions_button">
+        {/* <button className="navBar" onClick={openMenu}>
+          <i className="fas fa-bars nav_bars_icon"></i>
+          <i className="fas fa-user-circle user_icon"></i>
+        </button> */}
         <button className="actions_menu" onClick={openMenu}>
           <i className="fas fa-bars nav_bars_icon"></i>
           <i className="fas fa-user-circle user_icon"></i>
         </button>
         {showMenu && (
           <div id="menu">
-            <div className="profile_options">
-              <NavLink to="/currentUser/properties">
-                <button onClick={showUsersProperties}>My Properties</button>
-              </NavLink>
-              <NavLink to="/currentUser/reviews">
-                <button onClick={showUsersReviews}>My Reviews</button>
-              </NavLink>
-              <NavLink to="/currentUser/bookings">
-                <button onClick={showUsersBookings}>My Bookings</button>
-              </NavLink>
-              <button onClick={logout}>Log Out</button>
+            <Link to="/currentUser/properties" id="dropdown">
+              My Properties
+            </Link>
+            <Link to="/currentUser/bookings" id="dropdown">
+              My Bookings
+            </Link>
+            <Link to="/currentUser/reviews" id="dropdown">
+              My Reviews
+            </Link>
+            <div onClick={logout} id="dropdown">
+              Log out
             </div>
-            <p>Hello {user.username}!</p>
-            <p>Email: {user.email}</p>
           </div>
         )}
       </div>
